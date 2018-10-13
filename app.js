cdd2dbee89e6b86c64a32731ba56fc4bee4839ef
Jared Diehl <jareddiehl96@gmail.com>
2018-10-13 10:00:19 -0500

    Starting development

--- app.js
+++ app.js
@@ -14,6 +14,34 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blog-home', (req, res) => {
+    res.render('blog-home');
+});
+
+app.get('/blog-single', (req, res) => {
+    res.render('blog-single');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements');
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu');
+});
+
+app.get('/team', (req, res) => {
+    res.render('team');
+});
+
 server.listen(PORT, HOST, () => {
     var env = app.get('env');
     console.log(`${env.charAt(0).toUpperCase() + env.substring(1)} app listening at http://${server.address().address}:${server.address().port}`);
